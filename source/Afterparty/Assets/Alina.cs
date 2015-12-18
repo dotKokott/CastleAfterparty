23fa53b14b320da3692edb53c06486f6329fbe49
Christian Kokott <christian.kokott@gmail.com>
2015-12-18 14:38:13 +0100

    people have needs

--- source/Afterparty/Assets/Alina.cs
+++ source/Afterparty/Assets/Alina.cs
@@ -1,20 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum Need {
+    Falafel,
+    Beer,
+    MDMA
+}
+
 public class Alina : MonoBehaviour {
 
     public GameObject Falafel;
     public GameObject Bottle;
     public GameObject Bag;
 
+    public Person CurrentTarget;
+    public Need CurrentNeed = Need.Falafel;
+
+    public Person[] People;
+
 	// Use this for initialization
 	void Start () {
-	
+        People = GameObject.FindObjectsOfType<Person>();
+
+        StartCoroutine( NewTarget(5) );
 	}
+
+    IEnumerator NewTarget(float wait) {
+        yield return new WaitForSeconds( wait );
+
+        var newPerson = People[Random.Range( 0, People.Length - 1 )];
+
+        CurrentTarget = newPerson;
+        CurrentNeed = (Need)Random.Range( 0, 2 );
+
+        Debug.Log( "New Target: " + CurrentTarget.Name + " " + CurrentNeed.ToString() );
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
     void OnControllerColliderHit( ControllerColliderHit hit ) {
@@ -38,9 +62,21 @@ public class Alina : MonoBehaviour {
 
         var person = hit.gameObject.GetComponent<Person>();
         if( person ) {
+            var hasFalafel = Falafel.activeInHierarchy;
+            var hasBottle = Bottle.activeInHierarchy;
+            var hasBag = Bag.activeInHierarchy;
+
             Falafel.SetActive( false );
             Bottle.SetActive( false );
             Bag.SetActive( false );
+
+            if((hasFalafel && CurrentNeed == Need.Falafel) || (hasBottle && CurrentNeed == Need.Beer) || (hasBottle && CurrentNeed == Need.MDMA)) {
+                if ( CurrentTarget == person ) {
+                    CurrentTarget = null;
+                    StartCoroutine( NewTarget( Random.Range( 1, 4 ) ) );
+                }
+            }
+
         }
     }
 }
