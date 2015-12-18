facc55b3a0bb89e5c0adf6e086fb15f8fb21f727
Christian Kokott <christian.kokott@gmail.com>
2015-12-18 15:50:59 +0100

    spotm oves

---
+++ source/Afterparty/Assets/SpotMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpotMove : MonoBehaviour {
+    
+    public Vector3 To;
+
+    public float Time;
+
+	// Use this for initialization
+	void Start () {
+        iTween.RotateTo( gameObject, iTween.Hash( "rotation", To, "time", Time, "looptype", iTween.LoopType.pingPong, "easetype", iTween.EaseType.easeInCubic ) );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
